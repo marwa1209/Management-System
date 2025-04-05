e8b6517af0f5ce8f13230a53057cb35621a4ad30
marwa172 <marwa@bluskyint.com>
2025-04-05 13:08:41 +0200

    login component

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FEAF00',
+        secondary: '#F8D442',
+      },
+    },
+  },
+  plugins: [],
+};
